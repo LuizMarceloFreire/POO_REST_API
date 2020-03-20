7b46ce5baec2e902e2e3c4a219d6804543355f2b
Luiz Marcelo Freire <luiz.marcelo-developer@outlook.com>
2020-03-20 14:01:11 -0400

    Add project files.

---
+++ POO_REST_API/Controllers/ClienteController.cs
@@ -0,0 +1,34 @@
+﻿using POO_REST_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace POO_REST_API.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        public List<Cliente> Get()
+        {
+            return clientes;
+        }
+
+        public void Post(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            {
+                clientes.Add(new Cliente(nome));
+            }
+            
+        }
+
+        public void Delete(string nome)
+        {
+            clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+        }
+    }
+}
